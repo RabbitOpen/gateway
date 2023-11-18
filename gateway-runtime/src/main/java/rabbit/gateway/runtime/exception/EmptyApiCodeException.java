1b62242be5a6a79e3bd74800030767a1be86af93
RabbitOpen <709498191@qq.com>
2023-11-18 13:35:28 +0800

    init

--- gateway-runtime/src/main/java/rabbit/gateway/runtime/exception/EmptyApiCodeException.java
+++ gateway-runtime/src/main/java/rabbit/gateway/runtime/exception/EmptyApiCodeException.java
@@ -5,6 +5,6 @@ import rabbit.gateway.common.exception.GateWayException;
 public class EmptyApiCodeException extends GateWayException {
 
     public EmptyApiCodeException(String uri) {
-        super(String.format("invalid request[%s], api code is not found!", uri));
+        super(String.format("非法请求[%s], api code不能为空!", uri));
     }
 }
